806bb32d57ce30672ddce59c6e9acbb5ecb4512d
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2023-10-21 15:27:33 +0530

    product listing and react router

---
+++ frontend/src/products.jsx
@@ -0,0 +1,17 @@
+const products = [
+    {
+        _id:1,
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'images/airpods.jpg',
+        description:"",
+        brand:"Apple",
+        category:'Electonics',
+        price:89.99,
+        countInStock:10,
+        rating:4.5,
+        numReviews:12,
+    }
+]
+
+
+export default products
