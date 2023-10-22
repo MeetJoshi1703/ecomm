76ee9c7fd244630fbf03e3d3bb8fd94d3488b428
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2023-10-22 15:22:22 +0530

    Adding ProductScreen page

--- frontend/src/products.jsx
+++ frontend/src/products.jsx
@@ -2,8 +2,8 @@ const products = [
     {
         _id:1,
         name:'Airpods Wireless Bluetooth Headphones',
-        image:'images/airpods.jpg',
-        description:"",
+        image:'/images/airpods.jpg',
+        description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
         brand:"Apple",
         category:'Electonics',
         price:89.99,
@@ -14,4 +14,4 @@ const products = [
 ]
 
 
-export default products
+export default products;
