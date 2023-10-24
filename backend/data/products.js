352bea93e77f8b75fe9c94eaac4f2d1db9a03e8c
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2023-10-24 20:34:22 +0530

    adding backend

---
+++ backend/data/products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        _id:1,
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'/images/airpods.jpg',
+        description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
+        brand:"Apple",
+        category:'Electonics',
+        price:89.99,
+        countInStock:10,
+        rating:4.5,
+        numReviews:12,
+    }
+]
+
+
+
+export default products;
