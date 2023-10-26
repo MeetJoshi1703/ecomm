20573b98b0c8cfca3f1e8ce240fea4a7a0721366
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2023-10-26 12:18:43 +0530

    adding database

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,14 +1,74 @@
 const products = [
     {
-        _id:1,
+        
         name:'Airpods Wireless Bluetooth Headphones',
         image:'/images/airpods.jpg',
         description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
         brand:"Apple",
-        category:'Electonics',
+        category:'Electronics',
         price:89.99,
         countInStock:10,
         rating:4.5,
+        numReviews:12,
+    },
+    {
+        
+        name:'Iphone 11 Pro 256GB Memory',
+        image:'/images/phone.jpg',
+        description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
+        brand:"Apple",
+        category:'Electronics',
+        price:599.99,
+        countInStock:7,
+        rating:4.0,
+        numReviews:8,
+    },
+    {
+        
+        name:'Cannon EOS 80D DSLR Camers',
+        image:'/images/camera.jpg',
+        description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
+        brand:"Cannon",
+        category:'Electronics',
+        price:929.99,
+        countInStock:5,
+        rating:3,
+        numReviews:12,
+    },
+    {
+        
+        name:'Sony Playstation 4 Pro White Version',
+        image:'/images/playstation.jpg',
+        description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
+        brand:"Sony",
+        category:'Electronics',
+        price:399.99,
+        countInStock:11,
+        rating:5,
+        numReviews:12,
+    },
+    {
+        
+        name:'Logitech G-series Gaming Mouse',
+        image:'/images/mouse.jpg',
+        description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
+        brand:"Logitech",
+        category:'Electronics',
+        price:49.99,
+        countInStock:7,
+        rating:3.5,
+        numReviews:8,
+    },
+    {
+        
+        name:'Amazon Echo Dot 3rd Generation',
+        image:'/images/alexa.jpg',
+        description:"Active Noise Cancellation reduces unwanted background noise Adaptive Transparency lets outside sounds in while reducing loud environmental noise Personalised Spatial Audio with dynamic head tracking places sound all around you",
+        brand:"Amazon",
+        category:'Electronics',
+        price:29.99,
+        countInStock:0,
+        rating:4,
         numReviews:12,
     }
 ]
