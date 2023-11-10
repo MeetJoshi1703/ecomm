001ba39c8f7f894196d36efc1d3c9285b11e6f10
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2023-11-11 02:48:07 +0530

    adding backend authentication

---
+++ backend/routes/uesrRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const router = express.Router();
+
+import {
+    authUser,
+    registerUser,
+    logOutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    getUserByID,
+    deleteUser,
+    updateUser
+} from '../controllers/userController.js';
+import { protect,Admin } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser).get(getUsers);
+
+router.post('/logout',logOutUser);
+router.post('/auth',authUser);
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile);
+
+router.route('/:id').delete(protect,Admin,deleteUser).get(protect,Admin,getUserByID).put(protect,Admin,updateUser);
+
+export default router;    
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { notFound,errorHandler } from './middleware/errorHandler.js';
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/uesrRoutes.js'
 import connectDB from './config/db.js'; 
+import cookieParser from 'cookie-parser';
 
 const port =process.env.PORT || 5000;
 
@@ -11,7 +13,13 @@ connectDB();
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
